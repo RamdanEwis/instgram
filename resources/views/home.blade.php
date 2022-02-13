61a6c2ba5d78082555cacedb13683134649d37d6
DevRamdanEwis <you@example.com>
2022-02-13 09:41:55 +0200

    create strcture project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-lg-5 text-center"><img src="png/17437862_1850644885174186_6218115877187878912_a.jpg" style="height: 154px" class="rounded-circle"  alt="">
+        </div>
+        <div class="col-lg-7">
+            <div class="pb-2 pl-0"><h3>RamdanEwis</h3></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong> 9  </strong> posts</div>
+                <div class="pr-5"><strong>425 </strong>followers</div>
+                <div class=""><strong> 430 </strong>following</div>
+            </div>
+            <div class="description pt-3">
+                <h4>RamdanEwis</h4>
+            </div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-lg-4"><img src="png/242222154_119007663828964_5138014553399567994_n.jpg"  alt=""></div>
+        <div class="col-lg-4"><img src="png/242222154_119007663828964_5138014553399567994_n.jpg"  alt=""></div>
+        <div class="col-lg-4"><img src="png/242222154_119007663828964_5138014553399567994_n.jpg"  alt=""></div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-lg-4"><img src="png/242222154_119007663828964_5138014553399567994_n.jpg"  alt=""></div>
+        <div class="col-lg-4"><img src="png/242222154_119007663828964_5138014553399567994_n.jpg"  alt=""></div>
+        <div class="col-lg-4"><img src="png/242222154_119007663828964_5138014553399567994_n.jpg"  alt=""></div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-lg-4"><img src="png/242222154_119007663828964_5138014553399567994_n.jpg"  alt=""></div>
+        <div class="col-lg-4"><img src="png/242222154_119007663828964_5138014553399567994_n.jpg"  alt=""></div>
+        <div class="col-lg-4"><img src="png/242222154_119007663828964_5138014553399567994_n.jpg"  alt=""></div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-lg-4"><img src="png/242222154_119007663828964_5138014553399567994_n.jpg"  alt=""></div>
+        <div class="col-lg-4"><img src="png/242222154_119007663828964_5138014553399567994_n.jpg"  alt=""></div>
+        <div class="col-lg-4"><img src="png/242222154_119007663828964_5138014553399567994_n.jpg"  alt=""></div>
+    </div>
+</div>
+@endsection
